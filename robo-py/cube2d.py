20813b1e77146aee60ef00851e0934376a99eaa4
Paweł Radomski <pawel.radomski@efl.com.pl>
2014-11-14 14:40:56 +0100

    cube2d

---
+++ robo-py/cube2d.py
@@ -0,0 +1,44 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_X0 = 17
+GPIO_X1 = 18
+GPIO_Y0 = 22
+GPIO_Y1 = 23
+
+print "Pomiar odleglosci"
+
+GPIO.setup(GPIO_X0, GPIO.OUT)
+GPIO.setup(GPIO_X1, GPIO.OUT)
+GPIO.setup(GPIO_Y0, GPIO.OUT)
+GPIO.setup(GPIO_Y1, GPIO.OUT)
+
+GPIO.output(GPIO_X0, False)
+GPIO.output(GPIO_X1, False)
+GPIO.output(GPIO_Y0, False)
+GPIO.output(GPIO_Y1, False)
+
+
+iterator = 0
+while iterator < 20:
+
+    # stan 1
+    GPIO.output(GPIO_X0, False)
+    GPIO.output(GPIO_X1, False)
+    GPIO.output(GPIO_Y0, True)
+    GPIO.output(GPIO_Y1, True)
+    print "WSZYSTKIE ZAPALONE!"
+    time.sleep(5)
+
+    # stan 2
+    GPIO.output(GPIO_X0, False)
+    GPIO.output(GPIO_X1, True)
+    GPIO.output(GPIO_Y0, False)
+    GPIO.output(GPIO_Y1, False)
+    print "PRAWA STRONA"
+    time.sleep(5)
+
+    iterator+=1
