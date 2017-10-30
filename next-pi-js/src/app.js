217406b040f43a15e7c62f4ba0969c8db05c29c9
Radomski, Pawel <radomskip@ryanair.com>
2017-10-30 22:13:49 +0100

    init

--- next-pi-js/src/app.js
+++ next-pi-js/src/app.js
@@ -1,19 +1,30 @@
 const gpio = require('pi-gpio');
 
 const PIN = 8;
+const PIN2 = 10;
 const INTERVAL = 500;
 
 var state = 0;
+
 function toggle() {
   return state = state ? 0 : 1;
 }
-gpio.open(PIN, 'output', function() {
-  setInterval(function() {
-    var newState = toggle();
-    console.log(newState);
-    gpio.write(PIN, newState, function() {
-      
-    });
-  }, INTERVAL);
+
+// gpio.open(PIN, 'output', function() {
+//   setInterval(function() {
+//     var newState = toggle();
+//     gpio.write(PIN, newState, function() {
+//
+//     });
+//   }, INTERVAL);
+// });
+gpio.open(PIN2, 'input', function () {
+  setInterval(function () {
+    gpio.read(PIN2, function (err, value) {
+      if (err) throw err;
+      console.log(value);
+    })
+  }, 100);
 });
+
 console.log('===> START <===');
