a1187231d3ba96e3ce0826e97cf4cd05707aff1c
p4welo <p4welo@gmail.com>
2017-10-30 21:02:06 +0000

    f

--- next-pi-js/src/app.js
+++ next-pi-js/src/app.js
@@ -1,10 +1,19 @@
 const gpio = require('pi-gpio');
 
-const PIN = 7;
-gpio.open(PIN, "output", function(err) {
-  gpio.write(PIN, 1, function() {
-    gpio.close(PIN);
-  });
-});
+const PIN = 8;
+const INTERVAL = 500;
 
-console.log('dupa');
+var state = 0;
+function toggle() {
+  return state = state ? 0 : 1;
+}
+gpio.open(PIN, 'output', function() {
+  setInterval(function() {
+    var newState = toggle();
+    console.log(newState);
+    gpio.write(PIN, newState, function() {
+      
+    });
+  }, INTERVAL);
+});
+console.log('===> START <===');
